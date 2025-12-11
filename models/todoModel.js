813a9d5335afc1088231ef35f96d0e3834cc8bfc
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-11 09:04:03 +0300

    create todo

--- models/todoModel.js
+++ models/todoModel.js
@@ -25,3 +25,17 @@ const getTodoById = async (id) => {
 };
 
 
+const createTodo = async (todoData) => {
+    try{
+        const { task, tags, status } = todoData;
+
+        const result = await pool.query(
+            'INSERT INTO todos (task, tags, status) VALUES ($1, $2, $3) RETURNING *',
+            [task, tags || [], status || 'todo']
+        );
+        return result.rows[0];
+    } catch (error){
+        throw error;
+    }
+}; 
+
