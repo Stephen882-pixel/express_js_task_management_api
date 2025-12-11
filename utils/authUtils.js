5cde03f1118219f80b3666ac3d6aee96b2340ef1
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-11 11:53:07 +0300

    compare passwords

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -11,3 +11,7 @@ const hashPassword = async (password) => {
     return await bcrypt.hash(password,saltRounds);
 };
 
+
+const comparePassword = async (password, hash) => {
+    return await bcrypt.compare(password,hash);
+};
