9739116fefd4889a104580194f2ed37199c8f999
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-11 11:55:58 +0300

    generate Access Token

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -15,3 +15,15 @@ const hashPassword = async (password) => {
 const comparePassword = async (password, hash) => {
     return await bcrypt.compare(password,hash);
 };
+
+
+const generateAccessToken = (userId,email) => {
+    return jwt.sign(
+        { userId, email },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_ACCESS_EXPIRY || 15 }
+    );
+};
+
+
+
