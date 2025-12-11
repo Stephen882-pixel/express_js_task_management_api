62d97ed750ae04df76ce61129fe18ccf28e46f51
Stephen882-pixel <stephenondeyo0@gmail.com>
2025-12-11 12:08:36 +0300

    is password strong

--- utils/authUtils.js
+++ utils/authUtils.js
@@ -77,5 +77,8 @@ const isValidEmail = (email) => {
 };
 
 
-
+const isStrongPassword = (password) => {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return passwordRegex.test(password);
+};
 
